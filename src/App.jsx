065894dd13d8a92ce413269c6bf92e74173be1cc
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-28 22:45:55 +0600

    feat: search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+// STEP:04 -> IMPORT THE SEACH COMPONENT
+import Search from './components/Search';
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
             <img src="./hero-img.png" alt="hero-banner" />
             {/* STEP:01-> CREATE A HEADER AND DUMMY SEARCH ELEMENT */}
             <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
-            <p>Search</p>
+            {/* STEP:3 -> IMPORT THE SEARCH COMPONENT */}
+            <Search/>
           </header>
         </div>
       </div>

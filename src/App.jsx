855a6e7a5172bad380b4bf60c6b4e2267a0e8ea1
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-29 00:42:00 +0600

    useEffect import for data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-// STEP:04 -> IMPORT THE SEACH COMPONENT
-import { useState } from 'react';
+// IMPORT THE SEACH COMPONENT
+import { useEffect, useState } from 'react';
 import Search from './components/Search';
 
+
 const App = () => {
-  // STEP:05-> CREATE A STATE TO PASS searchTerm PROPS TO SEARCH BAR CHILD COMPONENT
+  // CREATE A STATE TO PASS searchTerm PROPS TO SEARCH BAR CHILD COMPONENT
 const [searchTerm, setSearchTerm] = useState('');
+
+// useEffect hook for movie fethching
+useEffect(() => {
+  
+}, [])
+
   return (
     <main>
       <div className="pattern">
         <div className="wrapper">
           <header>
-             {/* STEP:02-> IMPORT HERO IMAGE */}
+             {/*  IMPORT HERO IMAGE */}
             <img src="./hero-img.png" alt="hero-banner" />
-            {/* STEP:01-> CREATE A HEADER AND DUMMY SEARCH ELEMENT */}
+            {/*  CREATE A HEADER AND DUMMY SEARCH ELEMENT */}
             <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
-            {/* STEP:3 -> IMPORT THE SEARCH COMPONENT, STEP:06-> PASS THE STATE PROPS */}
+            {/* IMPORT THE SEARCH COMPONENT,  PASS THE STATE PROPS */}
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/> 
+
           </header>
         </div>
       </div>

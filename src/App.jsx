694afbb77520fec49b1605dbe3f530aca6514ba6
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-28 22:37:38 +0600

    feat: add hero background

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ const App = () => {
       <div className="pattern">
         <div className="wrapper">
           <header>
+             {/* STEP:02-> IMPORT HERO IMAGE */}
+            <img src="./hero-img.png" alt="hero-banner" />
+            {/* STEP:01-> CREATE A HEADER AND DUMMY SEARCH ELEMENT */}
             <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
             <p>Search</p>
           </header>

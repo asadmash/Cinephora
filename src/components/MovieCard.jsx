07dde2ae104d2de99fcc578cb39aae22b48200f9
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-29 02:41:22 +0600

    populate movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,9 +5,33 @@ const MovieCard = ({
 }) => {
   return (
     <div className="movie-card">
-        <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-poster.png'} alt="poster" />
+      <img
+        src={
+          poster_path
+            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+            : "/no-poster.png"
+        }
+        alt={title}
+      />
+      {/* movie title */}
+      <div className="mt-4">
+        <h3>{title}</h3>
+        <div className="content">
+          {/* rating */}
+          <div className="rating">
+            <img src="/rating.svg" alt="star" />
+            <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
+          </div>
+          <span>○</span>
+          <p className="lang">{original_language}</p>
+          <span>○</span>
+          <p className="year">
+            {release_date ? release_date.split("-")[0] : "N/A"}
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default MovieCard;

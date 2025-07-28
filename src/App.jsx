2c54c19fea181424b33061ab1db85acea45db437
asad.mash <92594708+asadmash@users.noreply.github.com>
2025-07-28 23:00:23 +0600

    pass state props from main page to search comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 // STEP:04 -> IMPORT THE SEACH COMPONENT
+import { useState } from 'react';
 import Search from './components/Search';
 
 const App = () => {
+  // STEP:05-> CREATE A STATE TO PASS searchTerm PROPS TO SEARCH BAR CHILD COMPONENT
+const [searchTerm, setSearchTerm] = useState('I am the one');
   return (
     <main>
       <div className="pattern">
@@ -11,8 +14,8 @@ const App = () => {
             <img src="./hero-img.png" alt="hero-banner" />
             {/* STEP:01-> CREATE A HEADER AND DUMMY SEARCH ELEMENT */}
             <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
-            {/* STEP:3 -> IMPORT THE SEARCH COMPONENT */}
-            <Search/>
+            {/* STEP:3 -> IMPORT THE SEARCH COMPONENT, STEP:06-> PASS THE STATE PROPS */}
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/> 
           </header>
         </div>
       </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-const Search = () => {
+const Search = ({searchTerm, setSearchTerm}) => { //STEP:02-> RECIVE THE PROPS AND DESTRUCTE IT
   return (
-    <div>Search</div>
+    // STEP:01-> STYPE THE SEARCH BAR TEXT
+    <div className='text-white text-3xl'>{searchTerm}</div>
   )
 }
 
